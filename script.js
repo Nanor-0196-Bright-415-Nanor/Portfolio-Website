171bffaa94de6a47c0f21969e29bd19f0cbd2716
Nanor-0196-Bright-415-Nanor <147738677+Nanor-0196-Bright-415-Nanor@users.noreply.github.com>
2024-05-12 02:37:02 +0200

    the sidebar

--- script.js
+++ script.js
@@ -6,6 +6,9 @@
  const twos = document.querySelectorAll(".two");
  const ones = document.querySelectorAll(".one");
  const defaultDispla = document.querySelector(".default-display");
+ const ulSide = document.getElementById("ulSide");
+ const menu = document.querySelector(".fa-bars");
+ const clse = document.querySelector(".fa-xmark")
 
   ones.forEach(function(one){
     one.addEventListener("click",function(){
@@ -21,8 +24,17 @@
     })
   });
 
+//Side bar
+//menu click
+menu.addEventListener("click",function eme(){
+  ulSide.style.width = "100px";
+})
 
+//Cross Click
 
+clse.addEventListener("click", function leks(){
+  ulSide.style.width = "0px";
+})
 
 
 
